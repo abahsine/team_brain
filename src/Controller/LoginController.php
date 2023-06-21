597342710d1837d49cb2d272f930e56f768009c2
asfolken <folken70@hotmail.com>
2023-06-21 22:29:15 +0200

    update

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class LoginController extends AbstractController
 {
@@ -31,13 +30,8 @@ class LoginController extends AbstractController
     }
 
     #[Route('/', name: 'home', methods: ['GET'])]
-    public function home(UserRepository $userRepository, SerializerInterface $serializer): Response
+    public function home(UserRepository $userRepository): Response
     {
-
-        $users = $userRepository->findAll();
-        dump($serializer->serialize($users, "json", ['groups' => ['user_detail']]));
-        die();
-
         return $this->redirectToRoute('projets');
     }
 }
